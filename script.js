0ca62d421fbb37ba20232f527e8228503ed72def
Kristina <kristina.dudnyk20@gmail.com>
2023-05-23 22:38:43 +0100

    episodesSearchBarFunction

--- script.js
+++ script.js
@@ -1,11 +1,101 @@
 import { fetchAllShows, fetchAllEpisodes } from "./data/fetched-data.js"; 
 
-function makePageForEpisodes(episodeList) {
 
-  console.log(episodeList);
+function episodesSearchBarFunction (allEpisodes) {
+  const selectEpisodeContainer = document.createElement("div");
 
-  const containerElement = document.getElementById("container");
+  const selectElement = document.createElement("select");
+  selectElement.className = "select-element";
+  selectElement.id = "select-element";
+  selectElement.name = "episodes";
+
+  for(let i = 0; i < allEpisodes.length; i++){
+    const optionElement = document.createElement("option");
+    optionElement.text = `S${allEpisodes[i].season.toString().padStart(2, "0")}E${allEpisodes[i].number.toString().padStart(2, "0")} - ${allEpisodes[i].name}`;
+    optionElement.value = allEpisodes[i].id;
+    selectElement.appendChild(optionElement);
+  }
+  selectElement.addEventListener("change", (event) => {
+    const chosenOption = event.target.value;
+    console.log(chosenOption)
+    const selectedEpisode = allEpisodes.filter(ep =>
+      // console.log(ep.id.includes(chosenOption))
+      ep.id === chosenOption
+    )
+    makePageForEpisodes(selectedEpisode)
+  })
   
+  selectEpisodeContainer.appendChild(selectElement)
+  return selectEpisodeContainer;
+}
+
+function searcAndCountFunction (allEpisodes) {
+  const searcAndCountContainer = document.createElement("div");
+  searcAndCountContainer.className = "searc-and-count-container";
+
+  const lebelForSearch = document.createElement("label");
+  lebelForSearch.for = "search-input";
+  lebelForSearch.className = "search-lebel"
+  lebelForSearch.textContent = "Search";
+
+  const searchInput = document.createElement("input");
+  searchInput.id = "search-input";
+
+  const countElementContainer = document.createElement("div");
+  const countElementItem = document.createElement("p");
+
+  countElementItem.textContent = `${allEpisodes.length}/${allEpisodes.length}`;
+
+  countElementContainer.appendChild(countElementItem);
+  searcAndCountContainer.appendChild(countElementContainer);
+
+  searchInput.addEventListener("input", (e) => {
+    const inputResult = e.target.value.trim();
+    console.log(inputResult);
+
+    const searchResult = allEpisodes.filter(
+      (key) => 
+      key.name.toLowerCase().includes(inputResult.toLowerCase()) ||
+      key.summary.toLowerCase().includes(inputResult.toLowerCase())
+    )
+    makePageForEpisodes(searchResult);
+
+    countElementItem.textContent = `${searchResult.length}/${allEpisodes.length}`;
+  })
+  searcAndCountContainer.appendChild(searchInput);
+
+  return searcAndCountContainer
+}
+
+
+function makeHeader(allEpisodes){
+  const header = document.createElement("header");
+  header.className = "header-container";
+
+  const title = document.createElement("h1");
+  title.className = "headers-title";
+  title.textContent = "TV-DOM-PROJECT";
+
+  const selectEpisodeMainContainer = document.createElement("div");
+  const selectEpisodeContainer = episodesSearchBarFunction(allEpisodes);
+  selectEpisodeMainContainer.appendChild(selectEpisodeContainer);
+  
+  const searcAndCountMainContainer = document.createElement("div");
+  const searcAndCountContainer = searcAndCountFunction(allEpisodes);
+  searcAndCountMainContainer.appendChild(searcAndCountContainer);
+  
+  header.appendChild(title);
+  header.appendChild(searcAndCountMainContainer);
+  header.appendChild(selectEpisodeMainContainer);
+  
+  document.body.appendChild(header);
+}
+
+
+function makePageForEpisodes(episodeList) {
+  const containerElement = document.getElementById("container");
+  containerElement.innerHTML = "";
+
   episodeList.forEach((episode) => {
     
     const episodeContainer = document.createElement("div");
@@ -36,34 +126,22 @@ function makePageForEpisodes(episodeList) {
   });
 }
 
-function makeFooter(episodeList) {
-  // create a footer and append it to the body
+
+
+function makeFooter(allEpisodes) {
   const footer = document.createElement("footer");
   footer.className = "footer-container";
   footer.innerHTML = 
     `Data Collected from TVMaze.com
     <br>
-    Found ${episodeList.length} episode(s)`;
+    Found ${allEpisodes.length} episode(s)`;
   document.body.appendChild(footer);
-}
-function makeHeader(allEpisodes){
-  const header = document.createElement("header");
-  header.className = "header-container";
-
-  // const searchDiv = document.cleateElement("div");
-  const input = document.createElement("input");
-  
-  header.appendChild(input)
-  // searchDiv.appendChild(input);
-
-  // header.appendChild(searchDiv);
-  document.body.appendChild(header);
 }
 
 
 async function initialise () {
-  const allShows = await fetchAllShows()
-  console.log(allShows);
+  // const allShows = await fetchAllShows()
+  // console.log(allShows);
 
   const allEpisodes = await fetchAllEpisodes(82);
   console.log(allEpisodes);
